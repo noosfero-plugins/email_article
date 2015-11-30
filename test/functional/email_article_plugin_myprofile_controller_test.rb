7fd68d0efd3fd811907c541ac5e5cc00d46cf356
Victor Costa <vfcosta@gmail.com>
2015-11-30 14:07:41 -0300

    Fix require of test helper

--- test/functional/email_article_plugin_myprofile_controller_test.rb
+++ test/functional/email_article_plugin_myprofile_controller_test.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__) + '/../../../../test/test_helper'
+require_relative '../../../../test/test_helper'
 
 class EmailArticlePluginMyprofileControllerTest < ActionController::TestCase
 
